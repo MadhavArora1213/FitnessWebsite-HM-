f3b0d0f258ffcb281dffcde12b03bfd438b7f409
MadhavArora1213 <madhavarora132005@gmail.com>
2024-07-05 19:14:07 +0530

    Add initial files

---
+++ Nutrition.js
@@ -0,0 +1,6 @@
+// const menuIcon = document.querySelector(".menu-icon");
+// const list = document.querySelector(".list");
+
+// menuIcon.addEventListener("click", () => {
+//   list.classList.toggle("active");
+// });
